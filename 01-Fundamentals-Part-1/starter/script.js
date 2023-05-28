adeb5a8d0baa7c262266a23651810b3a24a997cb
Ali Makhdoom <14muhammad@gmail.com>
2023-05-28 19:57:47 +0500

    call(), apply() and, bind() methods

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -433,3 +433,79 @@ var obj2 = {name:"akshay"};
 obj2.getAddress();
 */
 
+/***************************************************************
+ *
+ * 11.  Self Invoking Functions?
+ ***************************************************************** */
+
+/***************************************************************
+ *
+ * 12.  call(), apply() and, bind() methods
+ ***************************************************************** */
+//call():
+//It’s a predefined method in javascript.
+// This method invokes a method (function) by specifying the owner object.
+
+/*
+function sayHello(){
+    return "Hello " + this.name;
+}
+var obj = {name: "Sandy"};
+console.log(sayHello.call(obj));
+*/
+
+//call() method allows an object to use the method (function) of another object.
+/*
+var person = {
+    age: 23,
+    getAge: function (){
+        return this.age;
+    }
+}
+var person2 = {
+    age: 54
+}
+console.log(person.getAge.call(person2));
+*/
+
+//call() accepts arguments:
+/*
+function saySomething(msg){
+    return this.name + " is " + msg;
+}
+person4 = {
+    name: "ali"
+}
+console.log(saySomething.call(person4,"Good"));
+*/
+//apply()
+//The apply method is similar to the call() method. The only difference is that,
+// call() method takes arguments separately whereas, apply() method takes arguments as an array.
+
+/*
+function saySomething(message){
+    return this.name + " is " + message;
+}
+var person4 = {name:  "John"};
+console.log(saySomething.apply(person4, ["awesome"]));
+*/
+//bind():
+//This method returns a new function, where the value of “this” keyword will be bound to the owner object, which is provided as a parameter.
+//  Example with arguments:
+
+var bikeDetails = {
+    displayDetails: function(registrationNumber,brandName){
+        return this.name+ " , "+ "bike details: "+ registrationNumber + " , " + brandName;
+    }
+}
+
+var person1 = {name:  "Vivek"};
+
+var detailsOfPerson1 = bikeDetails.displayDetails.bind(person1, "TS0122", "Bullet");
+
+// Binds the displayDetails function to the person1 object
+
+
+console.log(detailsOfPerson1());
+//Returns Vivek, bike details: TS0122, Bullet
+
