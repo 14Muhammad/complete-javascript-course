b1b56da8fa87ec8be62d6ef637e9e9ef10326c60
Ali Makhdoom <14muhammad@gmail.com>
2023-05-30 22:55:52 +0500

    rest parameter and spread operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -900,3 +900,71 @@ const y = 23;
 y = 44; // Throws an error
 */
 
+/***************************************************************
+ *
+ * 19. rest parameter and spread operator
+ * Both rest parameter and spread operator were introduced in the ES6 version of javascript.
+ *
+ * Rest parameter ( … ):
+ *
+ * It provides an improved way of handling the parameters of a function.
+ * Using the rest parameter syntax, we can create functions that can take a variable number of arguments.
+ * Any number of arguments will be converted into an array using the rest parameter.
+ * It also helps in extracting all or some parts of the arguments.
+ * Rest parameters can be used by applying three dots (...) before the parameters.
+ * **Note- Rest parameter should always be used at the last parameter of a function:
+ * ***************************************************************** */
+/*
+function addAllArgs(...args){
+    let sum = 0;
+    let i = 0;
+    while(i<args.length){
+        sum += args[i];
+        i++;
+    }
+    return sum;
+}
+
+console.log(addAllArgs(1,2,3,1,1,1));
+
+// Incorrect way to use rest parameter
+function randomFunc(a,...args,c){
+//Do something
+}
+
+// Correct way to use rest parameter
+function randomFunc2(a,b,...args){
+//Do something
+}
+*/
+//Spread operator (…): Although the syntax of the spread operator is exactly the same as the rest parameter,
+// the spread operator is used to spreading an array, and object literals.
+// We also use spread operators where one or more arguments are expected in a function call.
+/*
+function addFourNumbers(num1,num2,num3,num4){
+    return num1 + num2 + num3 + num4;
+}
+
+let fourNumbers = [5, 6, 7, 8];
+
+
+console.log(addFourNumbers(...fourNumbers));
+// Spreads [5,6,7,8] as 5,6,7,8
+
+let array1 = [3, 4, 5, 6];
+let clonedArray1 = [...array1];
+//let clonedArray1 = array1;
+// Spreads the array into 3,4,5,6
+console.log(clonedArray1); // Outputs [3,4,5,6]
+let obj1 = {x:'Hello', y:'Bye'};
+let clonedObj1 = {...obj1}; // Spreads and clones obj1
+console.log(obj1);
+
+let obj2 = {z:'Yes', a:'No'};
+let mergedObj = {...obj1, ...obj2}; // Spreads both the objects and merges it
+console.log(mergedObj);
+// Outputs {x:'Hello', y:'Bye',z:'Yes',a:'No'};
+*/
+// Note- Key differences between rest parameter and spread operator:
+// Rest parameter is used to take a variable number of arguments and turns them into an array while the spread operator takes an array or an object and spreads it
+// Rest parameter is used in function declaration whereas the spread operator is used in function calls.
